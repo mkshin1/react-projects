484c779f71e99a23b9659bd4484cfc581e1b8e4b
Michael Shin <michaelshin@Admins-MacBook-Pro.local>
2021-03-16 11:24:46 -0700

    completed to 5-menu

--- 2-tours/setup/src/Tours.js
+++ 2-tours/setup/src/Tours.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({ tours, removeTour }) => {
+
+  console.log(tours, "this tours object is coming from app.js")
+  return <section>
+    <div className="title">
+      <h2>Our tours</h2>
+      <div className="underline"></div>
+    </div>
+    {
+      tours.map((tour) => {
+        return <Tour key={tour.key} {...tour} removeTour={removeTour}></Tour>
+      })
+    }
+
+  </section>;
 };
 
 export default Tours;
--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ id, info, title }) => {
+
+  const [showInfo, setShowInfo] = useState(false)
+
+  return <article className="question">
+
+    <header>
+      <h2>{title}</h2>
+      <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+        {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+
+      </button>
+
+
+    </header>
+    {
+      showInfo && <p>{info}</p>
+    }
+    {/* <p>{info}</p> */}
+
+  </article>;
 };
 
 export default Question;
